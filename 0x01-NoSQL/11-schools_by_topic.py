7133065f5a3290e3de6bb63ce6c35cf85c8f1b43
ndiritu-prisca <priscandiritu@outlook.com>
2023-10-18 01:19:01 +0300

    Schools by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A schools_by_topic module"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    A function that returns the list of school having a specific topic
+    """
+    results = mongo_collection.finc({"topics": topic})
+    return list(results)
