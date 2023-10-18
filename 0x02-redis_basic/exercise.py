1a4ca44ff9ff68cde1a9176fe6d20298bef4d313
ndiritu-prisca <priscandiritu@outlook.com>
2023-10-18 22:30:34 +0300

    Reading from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,38 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Callable = None):
+        """
+        A method that take a key string argument and an optional Callable
+        argument named fn
+        """
+        if self._redis.exists(key):
+            data = self._redis.get(key)
+            if fn:
+                return fn(data)
+            return data
+        return None
+
+    def get_str(self, key: str):
+        """Method that gets a string"""
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str):
+        """Method that gets an int"""
+        return self.get(key, fn=int)
+
+
+# Test cases
+cache = Cache()
+
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
